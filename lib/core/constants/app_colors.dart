0d48cf0133f55b5d70b07b54bcecac1b3c72fc21
Mohd Raza Shaikh <mohdraza7058@gmail.com>
2024-11-06 23:59:53 +0530

    adding app color

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColors {
+  static const primary = Color(0xff075E54);
+  static const secondary = Color(0xff128C7E);
+  static const geen = Color(0xff25D366);
+  static const teaGreen = Color(0xffDCF8C6);
+  static const blue = Color(0xff34B7F1);
+  static const whiteCololate = Color(0xffECE5DD);
+}
