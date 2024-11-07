ddde0f9bc00542f916b087cd74a480a69fdccede
Mohd Raza Shaikh <mohdraza7058@gmail.com>
2024-11-07 19:03:29 +0530

    bottom navigation bar added

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -10,6 +10,41 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: _currentIndex);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  final List<Widget> _pages = const [
+    Center(child: Text('Chats')),
+    Center(child: Text('Updates')),
+    Center(child: Text('Communities')),
+    Center(child: Text('Calls')),
+  ];
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+    _pageController.jumpToPage(index);
+  }
+
+  void _onPageChanged(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,8 +79,40 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
-      body: const Center(
-        child: Text('home page'),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: _onPageChanged,
+        children: _pages,
+      ),
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+        ),
+        child: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _currentIndex,
+          onTap: _onTabTapped,
+          items: const [
+            BottomNavigationBarItem(
+              label: 'Chats',
+              icon: Icon(Icons.chat),
+            ),
+            BottomNavigationBarItem(
+              label: 'Updates',
+              icon: ImageIcon(AssetImage('assets/icons/updates.png')),
+            ),
+            BottomNavigationBarItem(
+              label: 'Communities',
+              icon: Icon(Icons.groups),
+            ),
+            BottomNavigationBarItem(
+              label: 'Calls',
+              icon: Icon(Icons.call),
+            ),
+          ],
+        ),
       ),
     );
   }
